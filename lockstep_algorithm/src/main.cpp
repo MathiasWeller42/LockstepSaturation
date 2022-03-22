82631c35c03a8f1ff74fc1cd59153c3c37775496
Anna Blume <ablumejakobsen@gmail.com>
2022-03-22 13:15:44 +0100

    Set up to use Sylvan

---
+++ lockstep_algorithm/src/main.cpp
@@ -0,0 +1,31 @@
+// Your First C++ Program
+#include <sylvan.h>
+#include <sylvan_table.h>
+#include <sylvan_obj.hpp>
+
+#include <iostream>
+
+int main() {
+
+    // Init LACE
+    lace_start(1, 1000000);
+
+    const size_t memory_bytes = 128u * 1024u * 1024u;
+
+    // Init Sylvan
+    sylvan::sylvan_set_limits(memory_bytes,      // Set memory limit
+                              6,                 // Set (exponent) of cache ratio
+                              0);                // Initialise unique node table to full size
+    sylvan::sylvan_set_granularity(1);
+    sylvan::sylvan_init_package();
+    sylvan::sylvan_init_bdd();
+
+
+    std::cout << "Hello World!";
+
+
+    sylvan::sylvan_quit();
+    lace_stop();
+    return 0;
+}
+
