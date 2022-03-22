108d59503839835fcddb3f6fd60e10d0351c5ae6
rsmj1 <rsmj98@gmail.com>
2022-03-22 12:07:30 +0100

    Safety push
    Co-authored-by: MathiasWeller42 <MathiasWeller42@users.noreply.github.com>
    Co-authored-by: Anna Blume Jakobsen <au651460@uni.au.dk>

---
+++ read.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+using namespace std;
+
+int main() {
+   // Create a text string, which is used to output the text file
+   string myText;
+
+   // Read from the text file
+   ifstream MyReadFile("PNMLFiles\\SimpleLoadBal\\PT\\simple_lbs-20.pnml");
+   cout << "Yo I'm here1337";
+
+   list<string> placeList = {};
+   list<string> transitionList = {};
+   list<string> arcList = {};
+   int noPlaces = 0;
+   int noTransitions = 0;
+   int noArcs = 0;
+
+   // Use a while loop together with the getline() function to read the file line by line
+   while (getline (MyReadFile, myText)) {
+      if(myText.rfind("<place ", 0) == 0) {
+         noPlaces++;
+         placeList.push_back(myText);
+      }
+      if(myText.rfind("<transition ", 0) == 0) {
+         noTransitions++;
+         transitionList.push_back(myText);
+      }
+      if(myText.rfind("<arc ", 0) == 0) {
+         noArcs++;
+         arcList.push_back(myText);
+      }
+   }
+
+   cout << endl;
+   cout << "Number of places: " << noPlaces << endl;
+   cout << "Number of places in placeList: " << placeList.size() << endl;
+   cout << "Number of transitions: " << noTransitions << endl;
+   cout << "Number of transitions in transitionList: " << transitionList.size() << endl;
+   cout << "Number of arcs: " << noArcs << endl;
+   cout << "Number of arcs in arcList: " << arcList.size() << endl;
+
+   // Close the file
+   MyReadFile.close();
+}
